21f6841804309cf4c22f8017f41c98c0188cab8b
Himanshu Trivedi <himanshutrivedi421@gmail.com>
2025-08-26 03:53:31 +0530

    Optimizing the code

--- Hacktalk Frontend/src/components/Body.jsx
+++ Hacktalk Frontend/src/components/Body.jsx
@@ -1,8 +1,43 @@
 
  import NavBar from "./NavBar"
   import Footer from "./Footer"
- import { Outlet } from "react-router-dom"
+ import { Outlet, useNavigate } from "react-router-dom"
+ import { BASE_URL } from "../utils/constants";
+import axios from "axios";
+import { useDispatch, useSelector } from 'react-redux';
+import {addUser} from"../utils/userSlice"
+import { useEffect } from "react";
+
 const Body = () => {
+  const dispatch=useDispatch();
+  const navigate=useNavigate();
+  const userData=useSelector(store=>store.user);
+  const fetchUser= async()=>{
+    try{
+        const res=await axios.get(BASE_URL+"/profile/view",{
+      withCredentials:true,
+    });
+    dispatch(addUser(res.data));
+
+
+    }
+    catch(error){
+      if(error.status===401){
+        navigate("/login");
+      }
+     
+      console.log(error);
+
+    }
+  
+  };
+  useEffect(()=>{
+    if(!userData){
+      fetchUser();
+    }
+    fetchUser();
+
+  },[]);
   return (
     <div>
      <NavBar/>
